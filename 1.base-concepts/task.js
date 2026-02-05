be5e55c6df26cba76708c15484bedf9608cc9d8e
Anton Borodin <bor183@mail.ru>
2026-02-05 21:24:50 +0500

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+  const d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return [];
+  }
+
+  if (d === 0) {
+    return [-b / (2 * a)];
+  }
+
+  const sqrtD = Math.sqrt(d);
+  const x1 = (-b + sqrtD) / (2 * a);
+  const x2 = (-b - sqrtD) / (2 * a);
+
+  return [x1, x2];
 }
