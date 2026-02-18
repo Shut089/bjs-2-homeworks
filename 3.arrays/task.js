067139e331ba6be8bd1255884b8839db1de1e9a9
Anton Borodin <bor183@mail.ru>
2026-02-18 20:54:59 +0500

    Add HW arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,29 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+   }
+
+  return arr1.every(function (element, index) {
+    return element === arr2[index];
+  })
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter(function (user) {
+    return user.gender === gender;
+  });
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const ages = filteredUsers.map(function (user) {
+    return user.age;
+  });
+
+  const totalAge = ages.reduce(function (sum, age) {
+    return sum + age;
+  }, 0);
+
+  return totalAge / ages.length;
 }
