62ccc309544317428cf5fbf75de4fb069549dd1d
Anton Borodin <bor183@mail.ru>
2026-02-26 12:22:06 +0500

    Add HW decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,60 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  const cache = [];
+
+  return function (...args) {
+    const hash = md5(args);
+
+    const cached = cache.find(item => item.hash === hash);
+
+    if (cached) {
+      console.log("Из кеша: " + cached.value);
+      return "Из кеша: " + cached.value;
+    }
+
+    const result = func(...args);
+
+    cache.push({
+      hash: hash,
+      value: result
+    });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  };
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  let isFirstCall = true;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (isFirstCall) {
+      func.apply(this, args);
+      wrapper.count++;
+      isFirstCall = false;
+      return;
+    }
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+      wrapper.count++;
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
